2675362408d2c6056cba6761a09fd30ccf70b031
maldestor95 <maldestor95@gmail.com>
2020-07-28 17:01:20 +0200

    add trace to development server

--- src/dev/server.js
+++ src/dev/server.js
@@ -4,6 +4,11 @@ const cors = require('cors')
 const port=2222
 const app= express()
 app.use(cors())
-
-app.get('/*',express.static('src/md'))
+const trace =function(req,res,next)
+{console.log(req.originalUrl)
+    next()
+    // console.log(__dirname)
+    // console.log(__dirname.replace('src\\dev','src\\md'))
+}
+    app.use(trace,express.static(__dirname.replace('src\\dev','src\\md')))
 app.listen(port,function(){console.log(`Server starter on ${port}`)})
